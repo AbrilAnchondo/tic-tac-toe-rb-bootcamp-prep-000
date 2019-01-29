31c05fe996afcf30ca33b28ff6c47c2712951430
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-01-29 22:25:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -125,11 +125,11 @@ def winner(board)
  def play(board)
    until over?(board)
     turn(board)
+    turn_count(board)
    end
    if won?(board)
     puts "Congratulations #{winner(board)}!"
    elsif draw?(board)
      puts "Cat's Game!"
    end
-   
 end
